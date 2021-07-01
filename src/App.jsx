0fe47d0219759feb9da9e018d6f09df7f53c7510
suge <849809724@qq.com>
2021-07-01 17:27:57 +0800

    src变换

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,11 @@
-import React from 'react'
-import Form from './components/Form'
-export default function App() {
-
-  const formList = [
-    { label: "名称", type: "input", prop: "name" },
-    { label: "密码", type: "password", prop: "password" },
-    { label: "年龄", type: "number", prop: "age" },
-    {
-      label: "性别",
-      type: "radio",
-      prop: "sex",
-      data: [
-        { label: "男", value: "1" },
-        { label: "女", value: "2" },
-      ],
-    },
-    {
-      label: "爱好",
-      type: "checkbox",
-      prop: "hobby",
-      data: [
-        { label: "篮球", value: "1" },
-        { label: "足球", value: "2" },
-        { label: "乒乓球", value: "3" },
-      ],
-    },
-    {
-      label: "大学",
-      type: "select",
-      prop: "education",
-      data: [
-        { label: "本科", value: "1" },
-        { label: "专科", value: "2" },
-        { label: "高中", value: "3" },
-      ],
-    },
-  ];
-  
-  function handleSubmit(val){
-    console.log(val)
+import React, { Component } from 'react'
+import TodoList from './components/TodoList'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <TodoList />
+      </div>
+    )
   }
-
-  return (
-    <div>
-      <Form formList={formList} handleSubmit={ handleSubmit }/>
-    </div>
-  )
 }
-
